022732340c132ec4961ae8db069c443a44978e4f
Sid Ghimire <siddharthaghimire@gmail.com>
2023-02-18 15:07:09 +0545

    update

---
+++ Writer/App.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import NfcManager, {NfcTech, Ndef} from 'react-native-nfc-manager';
+
+// Pre-step, call this before any NFC operations
+NfcManager.start();
+
+function App() {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      const text = tag?.ndefMessage[0].payload.reduce(
+        (acc, byte) => acc + String.fromCharCode(byte),
+        '',
+      );
+      console.warn('Tag found', text);
+    } catch (ex) {
+      console.warn('Oops!', ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
+
+  return (
+    <View style={styles.wrapper}>
+      <TouchableOpacity onPress={readNdef}>
+        <Text>Scan a Tag</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+async function writeNdef() {
+  let result = false;
+
+  try {
+    // STEP 1
+    await NfcManager.requestTechnology(NfcTech.Ndef);
+
+    const bytes = Ndef.encodeMessage([Ndef.textRecord('Hello NFC')]);
+
+    if (bytes) {
+      await NfcManager.ndefHandler // STEP 2
+        .writeNdefMessage(bytes); // STEP 3
+      result = true;
+    }
+  } catch (ex) {
+    console.warn(ex);
+  } finally {
+    // STEP 4
+    NfcManager.cancelTechnologyRequest();
+  }
+
+  return result;
+}
+export default App;
